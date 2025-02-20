09aa601e610e477a0539d74e57ef349b8aebcce1
geovane-guerreiro <geovane.guerreiro0@gmail.com>
2025-02-20 08:17:24 -0300

    Update and rename 5Fibonaci.js to 4Fibonaci.js

---
+++ BackEnd/Atividade2/4Fibonaci.js
@@ -0,0 +1,10 @@
+const numero = 5;
+let sequencia = [0,1];
+
+
+for(var i = 0; i <= numero; i++){
+    const formula = sequencia[sequencia.length-1] + sequencia[sequencia.length-2]
+    sequencia.push(formula)
+    console.log(sequencia)
+
+}
