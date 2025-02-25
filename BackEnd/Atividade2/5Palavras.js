b4c89f16dddb49f7b4694836738e26d7e9f517f8
geovane-guerreiro <geovane.guerreiro0@gmail.com>
2025-02-25 08:20:14 -0300

    Update 5Palavras.js

--- BackEnd/Atividade2/5Palavras.js
+++ BackEnd/Atividade2/5Palavras.js
@@ -1,13 +1,16 @@
-const texto = "banana";
-const letras = texto.split("");
-const Separado = [];
-
-for(const letra of letras){
-    if(Separado[letra]){
-        Separado[letra] += 1;
-    }else{
-        Separado[letra] = 1;
+function ContaLetras(texto){
+    const letras = texto.split("");
+    const Separado = [];
+    for(const letra of letras){
+        if(Separado[letra]){
+            Separado[letra] += 1;
+        }else{
+            Separado[letra] = 1;
+        }
     }
+    return Separado;
+
 }
 
-console.log(Separado);
+
+console.log(ContaLetras("banana"));
