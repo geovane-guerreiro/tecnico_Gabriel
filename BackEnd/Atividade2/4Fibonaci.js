7dc7f8364f5350e3c344bb1ae60a3d5801d1cd71
geovane-guerreiro <geovane.guerreiro0@gmail.com>
2025-02-25 07:23:56 -0300

    Update 4Fibonaci.js
    arrumando código

--- BackEnd/Atividade2/4Fibonaci.js
+++ BackEnd/Atividade2/4Fibonaci.js
@@ -3,8 +3,7 @@ let sequencia = [0,1];
 
 
 for(var i = 0; i <= numero; i++){
-    const formula = sequencia[sequencia.length-1] + sequencia[sequencia.length-2]
-    sequencia.push(formula)
-    console.log(sequencia)
-
+    const formula = sequencia[sequencia.length-1] + sequencia[sequencia.length-2];
+    sequencia.push(formula);
 }
+    console.log(sequencia);
