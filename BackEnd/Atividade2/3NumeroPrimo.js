6922bf84d39e10c8e5df74d916cdce9ca4fde74c
geovane-guerreiro <geovane.guerreiro0@gmail.com>
2025-02-25 08:19:03 -0300

    Update 3NumeroPrimo.js

--- BackEnd/Atividade2/3NumeroPrimo.js
+++ BackEnd/Atividade2/3NumeroPrimo.js
@@ -1,18 +1,17 @@
-
-primo(4);
-
 function primo(numero){
     let verificador = 0;
-
+    
     for(let i = numero; i >= 1; i--){
         if(numero % i == 0){
             verificador += 1;
         }
     }
     if(verificador == 2){
-       return console.log("O numero ", numero," é primo.");
+        return "É primo.";
     }else{
-        return console.log("O numero ", numero," não é primo.");
+        return "Não é primo.";
     }
-
+    
 }
+
+console.log(primo(4));
