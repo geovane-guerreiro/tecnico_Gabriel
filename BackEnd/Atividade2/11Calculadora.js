b508df6b6db9d50ba8939da5a9dcfc54954da05e
geovane-guerreiro <geovane.guerreiro0@gmail.com>
2025-02-27 08:53:32 -0300

    Add files via upload

---
+++ BackEnd/Atividade2/11Calculadora.js
@@ -0,0 +1,36 @@
+let num1;
+let num2;
+
+function Soma(){
+    return num1 + num2;
+}
+
+function Subtracao(){
+    return num1 - num2;
+}
+
+function Divisao(){
+    return num1 / num2;
+}
+function RestoDivisao(){
+    return num1 % num2;
+}
+
+
+function Multiplicacao(){
+return num1 * num2;
+}
+
+
+num1 = 5;
+num2 = 2;
+
+console.log()
+console.log("O resultado da soma é ",Soma());
+console.log("-------------------------")
+console.log("O resultado da subtração é ",Subtracao());
+console.log("-------------------------")
+console.log("O resultado da divisão é ", Divisao(), " e o resto é ", RestoDivisao());
+console.log("-------------------------")
+console.log("O resultado da multiplicação é ", Multiplicacao());
+console.log("")
---
+++ BackEnd/Atividade2/9OrdenaArray.js
@@ -0,0 +1,4 @@
+function ordenaArray(arr){
+    return arr.sort((a, b) => a-b);
+}
+console.log(ordenaArray([2,8,4,9,12,5]));
